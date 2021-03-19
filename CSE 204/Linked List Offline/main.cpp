e3d51a3d9176778cb3035048d28212e8bd265f90
Mahdee Mushfique Kamal <mmkdhrubo@gmail.com>
2021-03-19 19:03:04 +0600

    19 mar

--- CSE 204/Linked List Offline/main.cpp
+++ CSE 204/Linked List Offline/main.cpp
@@ -1,4 +1,4 @@
-#include<bits/stdc++.h>
+#include<iostream>
 #define watch(x) cout<<(#x)<<" is "<<(x)<<endl;
 
 using namespace std;
@@ -20,13 +20,14 @@ class PillowGame{
 public:
     PillowGame();
     int pillowHolderId();
+    int pillowHolderReflx();
     void revDir();
     void pass();
     void addPlayer(int r);
-
-    void enterPlayer(int r){
-
-    }
+    void enterPlayer(int r);
+    void sequence();
+    void eleminate();
+    int getLen(){return len;}
 
 };
 
@@ -38,15 +39,38 @@ int main()
     for(int i=0;i<n;i++)cin>>R[i];
     PillowGame PG;
     for(int i=0;i<n;i++)PG.addPlayer(R[i]);
-    cout<<PG.pillowHolderId()<<" is holding the pillow"<<endl;
-    PG.pass();
-    cout<<PG.pillowHolderId()<<" is holding the pillow"<<endl;
-    PG.pass();
-    PG.pass();
-    PG.pass();
-    PG.revDir();
-    PG.pass();
-    cout<<PG.pillowHolderId()<<" is holding the pillow"<<endl;
+    int time = 0,t;
+    char command;
+    while(1){
+        cin>>t;
+        cin>>command;
+
+        while(time + PG.pillowHolderReflx() < t){
+            time+= PG.pillowHolderReflx();
+            PG.pass();
+        }
+        if(command == 'F'){
+            if(PG.getLen()!=1){
+                cout<<"Game Over : Player "<< PG.pillowHolderId() <<" is holding the pillow at t="<<t<<", ";
+                PG.sequence();
+            }
+            break;
+        }
+        else if(command == 'P'){
+            if(PG.getLen()!=1)cout<<"Player "<< PG.pillowHolderId() <<" is holding the pillow at t="<<t<<endl;
+        }
+        else if (command == 'R')PG.revDir();
+        else if (command == 'I'){
+            int x;
+            cin>>x;
+            PG.enterPlayer(x);
+        }
+        else if(command == 'M'){
+            if(PG.getLen()!=1)cout<<"Player 2 has been eliminated at t="<<t<<endl;
+            PG.eleminate();
+            time = t;
+        }
+    }
     return 0;
 }
 
@@ -71,6 +95,10 @@ int PillowGame::pillowHolderId(){
     return pillow->id;
 }
 
+int PillowGame::pillowHolderReflx(){
+    return pillow->reflx;
+}
+
 void PillowGame::pass(){
     if(direction == 1){
         pillow = pillow->next;
@@ -79,7 +107,21 @@ void PillowGame::pass(){
 }
 
 void PillowGame::addPlayer(int r){
-    if(len == 0){
+    if(len > 1){
+        Node *new_node = new Node;
+        new_node->id = ++id_gen;
+        new_node-> reflx = r;
+        len++;
+
+        tail->next = new_node;
+        new_node-> prev = tail;
+
+        new_node->next = head;
+        head->prev = new_node;
+
+        tail = new_node;
+    }
+    else if(len == 0){
         head->id = ++id_gen;
         head->reflx = r;
         len++;
@@ -94,18 +136,73 @@ void PillowGame::addPlayer(int r){
         head->prev = tail;
         tail->prev = head;
     }
-    else if(len > 1){
-        Node *new_node = new Node;
-        new_node->id = ++id_gen;
-        new_node-> reflx = r;
-        len++;
+}
 
-        tail->next = new_node;
-        new_node-> prev = tail;
+void PillowGame::enterPlayer(int r){
+    if(len==1)return;
+    Node *new_node = new Node;
+    new_node->id = ++id_gen;
+    new_node->reflx = r;
+    len++;
 
-        new_node->next = head;
-        head->prev = new_node;
+    if(direction == 1){
+        Node *left;
+        //Node *left = new Node;
+        left = pillow->prev;
 
-        tail = new_node;
+        left->next  = new_node;
+        new_node->prev = left;
+
+        new_node->next = pillow;
+        pillow->prev = new_node;
+
     }
+    else if(direction == -1){
+        Node *right;
+        //Node *right = new Node;
+        right = pillow->next;
+
+        pillow->next = new_node;
+        new_node->prev = pillow;
+
+        new_node->next = right;
+        right->prev = new_node;
+    }
+}
+
+
+void PillowGame::sequence(){
+    cout<<"pillow passing sequence = Player "<<pillow->id;
+    Node *temp;
+    if(direction == 1){
+        temp = pillow->next;
+        while(temp!=pillow){
+            cout<<" , "<<temp->id;
+            temp = temp-> next;
+        }
+    }
+    else{
+        temp = pillow->prev;
+        while(temp!=pillow){
+            cout<<" , "<<temp->id;
+            temp = temp-> prev;
+        }
+    }
+}
+
+
+void PillowGame::eleminate(){
+    if(len==1)return;
+    Node *left,*right;
+    left= pillow->prev;
+    right = pillow->next;
+
+    left->next = right;
+    right->prev = left;
+
+    if(direction==1)pillow = right;
+    else pillow = left;
+
+    len--;
+    if(len==1)cout<<"Game over : Player "<< pillow->id <<" wins!!"<<endl;
 }
