f7ac2a3edcb71b26b44edc18fdb90f380e76f6a5
Mahdee Mushfique Kamal <mmkdhrubo@gmail.com>
2021-03-26 18:24:08 +0600

    26 mar

--- CSE 204/Stack Queue Offline/stack_offline_stl.cpp
+++ CSE 204/Stack Queue Offline/stack_offline_stl.cpp
@@ -1,6 +1,8 @@
 #include<bits/stdc++.h>
 using namespace std;
 #define watch(x) cout<<(#x)<<" is "<<(x)<<endl;
+int presidence(char c);
+int stringtonum(string str);
 
 int main(){
     string expression;
@@ -13,12 +15,12 @@ int main(){
         else if(expression[i]==')')open--;
 
         if(open < 0){
-            cout<<"Invalid Expression"<<endl;
+            cout<<"Not valid."<<endl;
             return 0;
         }
     }
     if(open != 0){
-        cout<<"Invalid Expression"<<endl;
+        cout<<"Not valid."<<endl;
         return 0;
     }
 
@@ -36,31 +38,113 @@ int main(){
         string temp = expression.substr(0,last_entry) + '0' + expression.substr(last_entry);
         expression = temp;
     }
-    cout<<"After Removing Unary Operator: "<<expression<<endl;
+    //cout<<"After Removing Unary Operator: "<<expression<<endl;
 
-
-    //Dijkstra's Two Stack;
+    //Infix to postfix:
     stack<char> ops;
     stack<int> value;
-
     for(int i=0;i<expression.length();i++){
-        if(expression[i]>='0' || expression[i]<='9'){
-            value.push(expression[i]-'0');
+        //openning bracket
+        if(expression[i]=='(') ops.push(expression[i]);
+
+        //value
+        else if(expression[i]>='0' && expression[i]<='9'){
+            int num_len = 1;
+            while(expression[i+ num_len]>='0' && expression[i+num_len]<='9')num_len++;
+            int num = stringtonum(expression.substr(i,num_len));
+            i+=(num_len-1);
+            //watch(num)
+            value.push(num);
         }
-        else if(expression[i] == '+' || expression[i] == '-' || expression[i] == '*' || expression[i] == '/' )ops.push(expression[i]);
-        else if(expression[i] == ')'){
-            int rhs = value.top(); value.pop();
-            int lhs = value.top(); value.pop();
-            watch(lhs)
-            watch(rhs)
-            char sign = ops.top();ops.pop();
-            watch(sign)
-            if(sign == '+') value.push(lhs + rhs);
-            else if(sign == '-') value.push(lhs - rhs);
-            else if(sign == '*') value.push(lhs * rhs);
-            else if(sign == '/') value.push(lhs / rhs);
+
+        //binary Operand
+        else if(expression[i] == '+' || expression[i] == '-' || expression[i] == '*' || expression[i] == '/' ){
+            if(!ops.empty() && presidence(ops.top()) > presidence(expression[i]) ){
+                if(value.empty()){
+                    cout<<"Not valid."<<endl;
+                    return 0;
+                }
+                int rhs = value.top(); value.pop();
+                if(value.empty()){
+                    cout<<"Not valid."<<endl;
+                    return 0;
+                }
+                int lhs = value.top(); value.pop();
+
+                if(ops.top()=='*') value.push(lhs*rhs);
+                else if(ops.top()=='/') value.push(lhs/rhs);
+
+                ops.pop();
+                ops.push(expression[i]);
+            }
+            else ops.push(expression[i]);
+        }
+        //closing bracket
+        else if(expression[i]==')'){
+            while(ops.top()!='('){
+                char c = ops.top();ops.pop();
+                if(value.empty()){
+                    cout<<"Not valid."<<endl;
+                    return 0;
+                }
+                int rhs = value.top(); value.pop();
+                if(value.empty()){
+                    cout<<"Not valid."<<endl;
+                    return 0;
+                }
+                int lhs = value.top(); value.pop();
+
+                if(c == '+') value.push(lhs+rhs);
+                else if(c == '-') value.push(lhs-rhs);
+                else if(c == '*') value.push(lhs*rhs);
+                else if(c == '/') value.push(lhs/rhs);
+            }
+            ops.pop();
         }
     }
-    cout<<value.top()<<endl;
-    main();
+    //End For
+    while(!ops.empty()){
+        char c = ops.top();ops.pop();
+        if(value.empty()){
+            cout<<"Not valid."<<endl;
+            return 0;
+        }
+        int rhs = value.top(); value.pop();
+        if(value.empty()){
+            cout<<"Not valid."<<endl;
+            return 0;
+        }
+        int lhs = value.top(); value.pop();
+
+        if(c == '+') value.push(lhs+rhs);
+        else if(c == '-') value.push(lhs-rhs);
+        else if(c == '*') value.push(lhs*rhs);
+        else if(c == '/') value.push(lhs/rhs);
+    }
+
+    if(value.size()==1)cout<<"Valid expression, Computed value:  "<< value.top();
+    else{
+        cout<<"Not valid."<<endl;
+    }
+    return 0;
+}
+
+
+
+
+
+int presidence(char c){
+    if(c=='*' || c=='/')return 2;
+    else if (c=='+' || c=='-') return 1;
+    else return 0;
+}
+
+
+int stringtonum(string str){
+    int num=0;
+    for(int i=0;i<str.length(); i++){
+        num*=10;
+        num += int(str[i]-'0');
+    }
+    return num;
 }
